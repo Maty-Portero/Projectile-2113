2c559438522249b436b1a2a85cd96e53463431c0
Matias Portero <porteromatias@gmail.com>
2024-05-29 14:54:11 -0300

    agrego el codigo funcional del juego, se mueve na mas

---
+++ Project/Game1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project
+{
+    public class Game1 : Game
+    {
+        Texture2D playerTexture;
+        Vector2 playerPosition;
+        float playerSpeed;
+
+        int deadZone;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                _graphics.PreferredBackBufferHeight / 2);
+            playerSpeed = 100f;
+
+            deadZone = 4096;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            playerTexture = Content.Load<Texture2D>("Player_Sprite");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            
+            //Keyboard
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                playerPosition.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                playerPosition.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                playerPosition.X -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Right))
+            {
+                playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (playerPosition.X > _graphics.PreferredBackBufferWidth - playerTexture.Width / 2)
+            {
+                playerPosition.X = _graphics.PreferredBackBufferWidth - playerTexture.Width / 2;
+            }
+            else if (playerPosition.X < playerTexture.Width / 2)
+            {
+                playerPosition.X = playerTexture.Width / 2;
+            }
+
+            if (playerPosition.Y > _graphics.PreferredBackBufferHeight - playerTexture.Height / 2)
+            {
+                playerPosition.Y = _graphics.PreferredBackBufferHeight - playerTexture.Height / 2;
+            }
+            else if (playerPosition.Y < playerTexture.Height / 2)
+            {
+                playerPosition.Y = playerTexture.Height / 2;
+            }
+
+            //Joystick
+            if (Joystick.LastConnectedIndex == 0)
+            {
+                JoystickState jstate = Joystick.GetState(0);
+
+                float updatedBallSpeed = playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (jstate.Axes[1] < -deadZone)
+                {
+                    playerPosition.Y -= updatedBallSpeed;
+                }
+                else if (jstate.Axes[1] > deadZone)
+                {
+                    playerPosition.Y += updatedBallSpeed;
+                }
+
+                if (jstate.Axes[0] < -deadZone)
+                {
+                    playerPosition.X -= updatedBallSpeed;
+                }
+                else if (jstate.Axes[0] > deadZone)
+                {
+                    playerPosition.X += updatedBallSpeed;
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(playerTexture, playerPosition, null, Color.White, 0f, new Vector2(playerTexture.Width / 2, playerTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f); _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
