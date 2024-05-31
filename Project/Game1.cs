034a4e1ab14de1f976a2e6f8215ccbd2e673813a
lautilolgamer <lautaro.rc2007@gmail.com>
2024-05-31 16:57:07 -0300

    Agregue 2 mecanicas: el ajustar velocidad con Shift y disparar con Z

--- Project/Game1.cs
+++ Project/Game1.cs
@@ -1,20 +1,63 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Project
 {
+    public class Bullet
+    {
+        public Vector2 Position;
+        public float Speed = 300f;
+        private Texture2D[] textures;
+        private int currentFrame;
+        private double animationTime;
+        private double timePerFrame = 0.1;
+
+        public Bullet(Vector2 position, Texture2D[] textures)
+        {
+            Position = position;
+            this.textures = textures;
+            currentFrame = 0;
+            animationTime = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Position.Y -= Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Update animation
+            animationTime += gameTime.ElapsedGameTime.TotalSeconds;
+            if (animationTime >= timePerFrame)
+            {
+                currentFrame = (currentFrame + 1) % textures.Length;
+                animationTime -= timePerFrame;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textures[currentFrame], Position, Color.White);
+        }
+    }
+
     public class Game1 : Game
     {
         Texture2D playerTexture;
         Vector2 playerPosition;
         float playerSpeed;
+        float slowSpeed;
 
         int deadZone;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // Bullet variables
+        Texture2D[] bulletTextures;
+        List<Bullet> bullets;
+        float shootCooldown;
+        float shootTimer;
 
         public Game1()
         {
@@ -25,23 +68,29 @@ namespace Project
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
                 _graphics.PreferredBackBufferHeight / 2);
-            playerSpeed = 100f;
+            playerSpeed = 200f;
+            slowSpeed = 100f;
 
             deadZone = 4096;
+
+            bullets = new List<Bullet>();
+            shootCooldown = 0.2f;
+            shootTimer = 0;
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
             playerTexture = Content.Load<Texture2D>("Player_Sprite");
+
+            // Load bullet textures
+            bulletTextures = new Texture2D[2];
+            bulletTextures[0] = Content.Load<Texture2D>("player_bulletone"); // Reemplaza con el nombre correcto
+            bulletTextures[1] = Content.Load<Texture2D>("player_bullettwo"); // Reemplaza con el nombre correcto
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,30 +98,37 @@ namespace Project
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            
-            //Keyboard
             var kstate = Keyboard.GetState();
+            float currentSpeed = kstate.IsKeyDown(Keys.LeftShift) || kstate.IsKeyDown(Keys.RightShift) ? slowSpeed : playerSpeed;
+
+            Vector2 direction = Vector2.Zero;
 
             if (kstate.IsKeyDown(Keys.Up))
             {
-                playerPosition.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                direction.Y -= 1;
             }
 
             if (kstate.IsKeyDown(Keys.Down))
             {
-                playerPosition.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                direction.Y += 1;
             }
 
             if (kstate.IsKeyDown(Keys.Left))
             {
-                playerPosition.X -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                direction.X -= 1;
             }
 
             if (kstate.IsKeyDown(Keys.Right))
             {
-                playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                direction.X += 1;
             }
+
+            if (direction != Vector2.Zero)
+            {
+                direction.Normalize();
+            }
+
+            playerPosition += direction * currentSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
             if (playerPosition.X > _graphics.PreferredBackBufferWidth - playerTexture.Width / 2)
             {
@@ -92,42 +148,48 @@ namespace Project
                 playerPosition.Y = playerTexture.Height / 2;
             }
 
-            //Joystick
-            if (Joystick.LastConnectedIndex == 0)
+            // Disparo
+            shootTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.Z) && shootTimer <= 0)
             {
-                JoystickState jstate = Joystick.GetState(0);
+                Shoot();
+                shootTimer = shootCooldown;
+            }
 
-                float updatedBallSpeed = playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-                if (jstate.Axes[1] < -deadZone)
+            // Actualizar balas
+            for (int i = bullets.Count - 1; i >= 0; i--)
+            {
+                bullets[i].Update(gameTime);
+                if (bullets[i].Position.Y < 0)
                 {
-                    playerPosition.Y -= updatedBallSpeed;
-                }
-                else if (jstate.Axes[1] > deadZone)
-                {
-                    playerPosition.Y += updatedBallSpeed;
-                }
-
-                if (jstate.Axes[0] < -deadZone)
-                {
-                    playerPosition.X -= updatedBallSpeed;
-                }
-                else if (jstate.Axes[0] > deadZone)
-                {
-                    playerPosition.X += updatedBallSpeed;
+                    bullets.RemoveAt(i);
                 }
             }
 
             base.Update(gameTime);
         }
 
+        private void Shoot()
+        {
+            Vector2 bulletPosition = new Vector2(playerPosition.X, playerPosition.Y - playerTexture.Height / 2);
+            Bullet newBullet = new Bullet(bulletPosition, bulletTextures);
+            bullets.Add(newBullet);
+        }
+
         protected override void Draw(GameTime gameTime)
         {
             _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(playerTexture, playerPosition, null, Color.White, 0f, new Vector2(playerTexture.Width / 2, playerTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f); _spriteBatch.End();
+            _spriteBatch.Draw(playerTexture, playerPosition, null, Color.White, 0f, new Vector2(playerTexture.Width / 2, playerTexture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
+
+            // Dibujar balas
+            foreach (var bullet in bullets)
+            {
+                bullet.Draw(_spriteBatch);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fa8112093e07418bf759802a6fa17da69f283d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa085292482eb5e7d2c4ec064273c9bffdd7773")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
