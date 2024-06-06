e39a4ba8e1be9998bf5b957d7ba5d15da658d1d2
unknown <porteromatias@gmail.com>
2024-06-06 13:46:04 -0300

    cambio la foto de porter

--- Project/obj/Release/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Release/net6.0/Project.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa085292482eb5e7d2c4ec064273c9bffdd7773")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
