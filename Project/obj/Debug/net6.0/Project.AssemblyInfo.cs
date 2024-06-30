e8829eb655e1733d186f26e5c79dca01c696b0a7
chokoterisaki <47866874+chokoterisaki@users.noreply.github.com>
2024-06-30 11:39:00 -0300

    test cache

--- Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c308de1d51b7d7de173808e334d34bb9487f66d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47ad8c268f369c283cc630f02223061f09f73118")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
