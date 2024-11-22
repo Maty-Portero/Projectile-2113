a4335a1ee358b20b22dc2cd5aee930643acd8e68
unknown <porteromatias@gmail.com>
2024-11-22 17:26:05 -0300

    actualizo a full la pag

--- Web/includes/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "projectile2113");
-
-if($conexion){
-    echo "conexion exitosa"."<br>";
-}else{
-    echo "conexion fallida"."<br>";
-}
-?>
