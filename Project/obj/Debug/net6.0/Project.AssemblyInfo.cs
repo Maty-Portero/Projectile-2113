c7d12fcda1d5cd8ce37ec04779d622a694badf36
Maty-Portero <porteromatias@gmail.com>
2024-11-21 22:09:30 -0300

    ns

--- Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f793a7cc0869f544bfbdff42f1e43f6e66b26f12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac850f355a4bd082b26edf6b8ef0a2bf24655c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
